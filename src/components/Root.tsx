e5451b576dbb1084618dfdca2828d101197568e7
keonoh00 <keonoh00@gmail.com>
2023-02-16 19:28:06 +0900

    Add ReactQueryDevtools

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,4 +1,5 @@
 import { Box } from "@chakra-ui/react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Outlet } from "react-router-dom";
 import RootHeader from "./RootHeader";
 
@@ -10,6 +11,7 @@ const Root = () => {
       <RootHeader />
 
       <Outlet />
+      <ReactQueryDevtools />
     </Box>
   );
 };
