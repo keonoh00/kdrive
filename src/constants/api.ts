3c30bea88afed4a83c7bcc9af94626227db3ac3f
keonoh00 <keonoh00@gmail.com>
2023-02-28 19:57:59 +0900

    Configure development and production deploy

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,6 +1,9 @@
-export const API_BASE_URL = 'http://127.0.0.1:8000/api/v1/';
+export const API_BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://127.0.0.1:8000/api/v1/"
+    : "https://kdrive.onrender.com/api/v1/";
 
 export const QUERY_KEYS = {
-  USER_PROFILE: 'USER_PROFILE',
-  DIRECTORY: 'DIRECTORY'
+  USER_PROFILE: "USER_PROFILE",
+  DIRECTORY: "DIRECTORY",
 };
